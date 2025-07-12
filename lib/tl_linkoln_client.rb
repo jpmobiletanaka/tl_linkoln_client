b9f46f2779baee1ec07eadde2ac495874b2986d6
Ivanov Alexander <ivalex87@mail.ru>
2025-07-12 09:03:22 +0200

    Add: require 'savon'

--- lib/tl_linkoln_client.rb
+++ lib/tl_linkoln_client.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+require 'savon'
+
 Dir[File.join(__dir__, 'tl_linkoln_client/common/*.rb')].sort.each { |file| require_relative file }
 Dir[File.join(__dir__, 'tl_linkoln_client/**/*.rb')].sort.each { |file| require_relative file }
